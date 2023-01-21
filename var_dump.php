6ce091b16be9ec85732e610a364c082cd93b0b9c
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-01-21 21:31:36 +0600

    data_type

--- var_dump.php
+++ var_dump.php
@@ -1,13 +1,15 @@
 
 <?php
-$name = "tresna";
-$num = 20;
+$name = "  tresna";
+$num = 20  ;
 
-var_dump($name);
+//var_dump($name);
 
 echo "</br>";
 
-var_dump($num);
+//var_dump($num);
+
+var_dump($num    , $name);
 
 
 
