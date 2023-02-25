e2d4292ff54fd25cda044445a6e2be8a49caad62
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-02-26 01:50:54 +0600

    file

--- fileUpload.php
+++ fileUpload.php
@@ -2,14 +2,43 @@
 
 if(isset($_POST['submit'])){
    // echo "OK";
-   echo $_FILES['upfile']['name'];
-   echo $_FILES['upfile']['tmp_name'];
-   
+  $fileName = $_FILES['upfile']['name'];
+  $tmploc   = $_FILES['upfile']['tmp_name'];
+  $fileType =  $_FILES['upfile']['type'];
+  $fileSize =   $_FILES['upfile']['size'];
+
+
+  $uploc = "images/".$fileName;
+
+  if($fileSize<200000){
+    
+  if($fileType =='image/jpeg'){
+    if(file_exists($uploc)){
+        echo "File alraedy exists";
+    }else{
+    }
+
+    if(move_uploaded_file($tmploc,$uploc)){
+        echo "Uploaded";
+}
+else{
+     echo "Not uploaded";
+}
+}else{
+    echo "enter select file";
 }
 
+  }
+  else{
+    echo "Size must be less than 200000";
+    
+  }
+  
 
-
-
+}
+    
+ 
+   
 
 ?>
 <form action = "<?php echo $_SERVER['PHP_SELF']; ?>"  method = "POST" enctype = "multipart/form-data">
