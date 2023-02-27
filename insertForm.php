ac508a77e735bbd2651884bf8caaa7978f458dcf
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-02-27 20:14:27 +0600

    form

---
+++ insertForm.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+?>
+<form action = "insertForm.php" method= "POST">
+    FirstName :<br>
+    <input type ="text" name = "FirstName"><br><br>
+   
+    LastName :<br>
+    <input type ="text" name = "LastName"><br><br>
+
+    Email :<br>
+    <input type ="email" name = "Email"><br><br>
+
+    <input type ="Submit"  value ="Submit" name = "submit"><br><br>
+</form>
