4cae7d6846857976043b117eb09a44bd034ef85b
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-03-05 06:46:47 +0600

    file

--- multipleFile.php
+++ multipleFile.php
@@ -1,7 +1,18 @@
 <?php
  if (isset($_POST['submit'])){
-    echo "OK";
+   $total = count($_FILES['file']['name']);
+
+   for($x=0;$x<$total;$x++){
+    $fileName =  $_FILES['file']['name'][0];
+     $tempName =  $_FILES['file']['tmp_name'][0];
+
+
+     $uploc = 'images/'.$fileName;
+
+     move_uploaded_file($tempName,$uploc);
+      
  }
+}
 
 ?>
 <form action= "multipleFile.php" method = "POST" enctype = "multipart/form-data">
