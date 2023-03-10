e78023ca90da8210993187f4ff50c9cdb4c1beef
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-03-10 11:07:41 +0600

    web

--- webSecurity.php
+++ webSecurity.php
@@ -1,7 +1,16 @@
 <?php
+if (isset($_POST['username'])){
+    echo $_POST['username'];
+}
 
 
 ?>
 
-<form action = "">
+<form action = "<?php echo $_SERVER ['PHP_SELF'];?>" method = "POST">
+UserName :<br>
+<input type = "text" name = "username"><br><br>
+Password :<br>
+<input type = "password" name = "password"><br><br>
+
+<input type = "submit" value = "Submit"><br><br>
 </form>
