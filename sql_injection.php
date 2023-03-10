18569d4c59736e9ebea902d8b62c1b18abde3c8f
Tresna <52813382+TresnaRani@users.noreply.github.com>
2023-03-10 18:42:35 +0600

    ss

--- sql_injection.php
+++ sql_injection.php
@@ -1,7 +1,12 @@
 <?php
 $conn = mysqli_connect('localhost','root','','phpmydb');
 
-   $sql =  "SELECT * FROM Users WHERE username = 'Tresna'";
+    if(isset($_POST['submit'])){
+        echo 'OK';
+    }
+  
+/*
+$sql =  "SELECT * FROM Users WHERE username = 'Tresna'";
    $query = mysqli_query($conn,$sql);
 
     if(mysqli_num_rows($query) >0){
@@ -10,6 +15,19 @@ $conn = mysqli_connect('localhost','root','','phpmydb');
         else{
             echo 'Invalid Username or Password';
         }
+        */
 
 
 ?>
+
+ <form action = "sql_injection.php" method = "POST">
+
+ Username :<br>
+ <input type = "text" name = "username"><br><br>
+
+ 
+ Password :<br>
+ <input type = "password" name = "password"><br><br>
+
+ <button type = "submit" name = "submit">Login</button>
+    </form>
